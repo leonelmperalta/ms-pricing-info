e4e46a1d314ad822afb77e67163c7c2970248539
Leonel Peralta <leonelm.peralta@outlook.es>
2024-08-25 16:12:00 -0300

    SpringBoot project creation.

---
+++ src/main/java/com/lperalta/ms/pricing/info/MsPricingInfoApplication.java
@@ -0,0 +1,13 @@
+package com.lperalta.ms.pricing.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPricingInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPricingInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lperalta/ms/pricing/info/MsPricingInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lperalta.ms.pricing.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPricingInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
