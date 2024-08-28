e001801482e0c83d06aca5b4428cda9b76ba47b3
Leonel Peralta <leonelm.peralta@outlook.es>
2024-08-28 20:25:12 -0300

    Auto generated code with Open Api Generator implemented.

--- src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/in/controller/PriceController.java
+++ src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/in/controller/PriceController.java
@@ -5,26 +5,13 @@ import com.lperalta.ms.pricing.info.prices.application.exception.NotDataFoundExc
 import com.lperalta.ms.pricing.info.prices.application.exception.PriceConfigurationErrorException;
 import com.lperalta.ms.pricing.info.prices.application.service.PriceQueryService;
 import com.lperalta.ms.pricing.info.prices.domain.model.PriceQuery;
-import com.lperalta.ms.pricing.info.prices.infraestructure.in.constants.ControllerConstants;
-import com.lperalta.ms.pricing.info.prices.infraestructure.in.constants.ValidationConstants;
-import com.lperalta.ms.pricing.info.prices.infraestructure.in.dto.PriceQueryResponseDTO;
+import com.lperalta.ms.pricing.info.prices.infraestructure.in.api.PriceControllerV1Api;
+import com.lperalta.ms.pricing.info.prices.infraestructure.in.dto.PriceQueryDTO;
 import com.lperalta.ms.pricing.info.prices.infraestructure.in.mapper.PriceQueryMapper;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(ControllerConstants.PRICE_CONTROLLER_BASE_PATH)
-@Validated
-public class PriceController {
+
+public class PriceController implements PriceControllerV1Api {
 
     private final PriceQueryService priceQueryService;
     private final PriceQueryMapper priceQueryMapper;
@@ -35,24 +22,9 @@ public class PriceController {
         this.priceQueryMapper = priceQueryMapper;
     }
 
-    @GetMapping
-    public ResponseEntity<PriceQueryResponseDTO> priceQuery(
-            @Valid
-            @RequestParam(name = ControllerConstants.BRAND_ID_QUERY_PARAM)
-            @NotNull(message = ValidationConstants.BRAND_ID_NOT_NULL)
-            @Positive(message = ValidationConstants.BRAND_ID_POSITIVE) Long brandId,
-            @Valid
-            @RequestParam(name = ControllerConstants.PRODUCT_ID_QUERY_PARAM)
-            @NotNull(message = ValidationConstants.PRODUCT_ID_NOT_NULL)
-            @Positive(message = ValidationConstants.PRODUCT_ID_POSITIVE) Long productId,
-            @Valid
-            @RequestParam(name = ControllerConstants.APPLICATION_DATE_QUERY_PARAM)
-            @NotBlank(message = ValidationConstants.APPLICATION_DATE_NOT_BLANK)
-            @Pattern(regexp = ValidationConstants.ISO_8601_FORMAT_REGEXP,
-                    message = ValidationConstants.APPLICATION_DATE_ISO_8601)
-            String applicationDate)
-            throws InternalServerErrorException, NotDataFoundException, PriceConfigurationErrorException {
-        PriceQuery priceQuery = this.priceQueryService.priceQuery(brandId, productId, applicationDate);
-        return ResponseEntity.ok(this.priceQueryMapper.toPriceQueryResponse(priceQuery));
+    @Override
+    public ResponseEntity<PriceQueryDTO> priceQuery(String applicationDate, Long productId, Long brandId)
+            throws PriceConfigurationErrorException, InternalServerErrorException, NotDataFoundException {
+        return null;
     }
 }
