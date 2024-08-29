a947d19809814ee0bf597e8197570b58d5a7103d
Leonel Peralta <leonelm.peralta@outlook.es>
2024-08-29 20:07:39 -0300

    Implementation of mapstruct for mapper.

--- src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/in/mapper/PriceQueryMapper.java
+++ src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/in/mapper/PriceQueryMapper.java
@@ -1,31 +1,21 @@
 package com.lperalta.ms.pricing.info.prices.infraestructure.in.mapper;
 
-import com.lperalta.ms.pricing.info.prices.application.mapper.DateConverter;
 import com.lperalta.ms.pricing.info.prices.domain.model.PriceQuery;
-import com.lperalta.ms.pricing.info.prices.infraestructure.in.dto.PriceQueryResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import com.lperalta.ms.pricing.info.prices.infraestructure.in.dto.PriceQueryDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 
-@Component
-public class PriceQueryMapper {
+@Mapper(componentModel = "spring")
+public interface PriceQueryMapper {
 
-    private final DateConverter dateConverter;
+    PriceQueryMapper INSTANCE = Mappers.getMapper(PriceQueryMapper.class);
 
-    @Autowired
-    public PriceQueryMapper(DateConverter dateConverter) {
-        this.dateConverter = dateConverter;
-    }
-
-
-    public PriceQueryResponseDTO toPriceQueryResponse(PriceQuery priceQuery) {
-        return PriceQueryResponseDTO.builder()
-                .applicationEndDate(this.dateConverter.toISO8601String(priceQuery.getApplicationEndDate()))
-                .applicationStartDate(this.dateConverter.toISO8601String(priceQuery.getApplicationStartDate()))
-                .brandId(priceQuery.getBrandId())
-                .currency(priceQuery.getCurrency())
-                .feeId(priceQuery.getFeeId())
-                .finalPrice(priceQuery.getFinalPrice())
-                .productId(priceQuery.getProductId())
-                .build();
-    }
+    @Mappings(value = {
+            @Mapping(source = "applicationEndDate", target = "startDate", dateFormat = "yyyy-MM-dd HH:mm:ss"),
+            @Mapping(source = "applicationStartDate", target = "endDate", dateFormat = "yyyy-MM-dd HH:mm:ss"),
+            @Mapping(source = "finalPrice", target = "finalAmount", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    })
+    PriceQueryDTO toDto(PriceQuery priceQuery);
 }
