16e5c67808b68a1a9a5f42da54e04403a54eb0ed
Leonel Peralta <leonelm.peralta@outlook.es>
2024-08-30 00:27:11 -0300

    Renamed class.

--- src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/out/database/repository/JPAPriceQueryRepository.java
+++ src/main/java/com/lperalta/ms/pricing/info/prices/infraestructure/out/database/repository/JPAPriceQueryRepository.java
@@ -1,6 +1,6 @@
 package com.lperalta.ms.pricing.info.prices.infraestructure.out.database.repository;
 
-import com.lperalta.ms.pricing.info.prices.infraestructure.out.database.dbo.SpringDataPriceEntity;
+import com.lperalta.ms.pricing.info.prices.infraestructure.out.database.dbo.JPAPriceQueryDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,13 +9,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface SpringDataPriceRepository extends JpaRepository<SpringDataPriceEntity, Long> {
-    @Query("SELECT e FROM SpringDataPriceEntity e " +
+public interface JPAPriceQueryRepository extends JpaRepository<JPAPriceQueryDAO, Long> {
+    @Query("SELECT e FROM JPAPriceQueryDAO e " +
             "WHERE e.productId = :productId " +
             "AND e.brandId = :brandId " +
             "AND e.applicationStartDate < :applicationDate " +
             "AND e.applicationEndDate > :applicationDate")
-    List<SpringDataPriceEntity> findByProductIdAndBrandIdAndApplicationDatesBetween(
+    List<JPAPriceQueryDAO> findByProductIdAndBrandIdAndApplicationDatesBetween(
             Long productId, Long brandId, LocalDateTime applicationDate
     );
 }
