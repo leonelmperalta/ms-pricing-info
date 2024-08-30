288f60aa5671b529fdff7b89b7cd78e8860c73ef
Leonel Peralta <leonelm.peralta@outlook.es>
2024-08-30 00:29:07 -0300

    Moved and renamed.

--- src/main/java/com/lperalta/ms/pricing/info/prices/domain/port/out/PriceQueryRepository.java
+++ src/main/java/com/lperalta/ms/pricing/info/prices/domain/port/out/PriceQueryRepository.java
@@ -1,12 +1,12 @@
-package com.lperalta.ms.pricing.info.prices.domain.repository;
+package com.lperalta.ms.pricing.info.prices.domain.port.out;
 
 import com.lperalta.ms.pricing.info.prices.domain.model.PriceQuery;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface PriceQueryRepositoryPort {
-    List<PriceQuery> findByProductIdAndBrandIdAndApplicationDatesBetween(
+public interface PriceQueryRepository {
+    List<PriceQuery> findPrices(
             Long productId, Long brandId, LocalDateTime applicationDate
     );
 }
